acd06f84ec3d89d05dca8fc9add8adb5290602b2
Kaushar45 <45kaushar@gmail.com>
2025-07-06 21:32:44 +0530

    use of middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,50 @@
 const express = require("express");
+const { writeFile } = require("node:fs/promises");
 const server = express();
 const port = 3000;
 
+server.use(express.json());
+
+server.use((req, res, next) => {
+  req.test = "test";
+  console.log("Global Middleware");
+  next();
+});
+
+server.post(
+  "/files",
+  (req, res, next) => {
+    console.log("local files Middleware");
+    next();
+  },
+  async (req, res) => {
+    console.log(res.query.name);
+    console.log(req.body);
+    console.log(req.test);
+
+    await writeFile(`./${req.query.name}.txt`, req.body.content);
+    res.json({ data: "files" });
+  }
+);
+
+server.use((req, res, next) => {
+  console.log(req.test);
+  next();
+});
 server.get("/", (req, res) => {
+  res.json("Hello root!");
+});
+server.get("/hello", (req, res) => {
   res.json("Hello World!");
 });
 
+server.post("/hello", (req, res) => {
+  res.json("Hello World!");
+});
 server.all(/^.*$/, (req, res) => {
+  res.json({
+    message: "Wrong Route",
+  });
   res.status(404).json("Not Found");
 });
 
