5694deeacbba8f8e4ad9ad88227bb94777054d81
Kaushar45 <45kaushar@gmail.com>
2025-07-10 19:40:58 +0530

    Add user data handling and validation in registerController

--- server/controllers/user.mjs
+++ server/controllers/user.mjs
@@ -1,6 +1,27 @@
 export const registerController = (req, res, next) => {
+  if (!req.body.username || !req.body.email || !req.body.password) {
+    res.status(400).json({
+      error: "Input is not valid.",
+    });
+    // throw new Error(JSON.stringify({ error: "input is not valid" }));
+    return;
+  }
+
+  const fileDataStr = readFile("./db.json", { encoding: "utf-8" });
+  const fileData = JSON.parse(fileDataStr);
+
+  const userData = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  fileData.user.push(userData);
+  writeFile("./db.json", JSON.stringify(fileData), {
+    encoding: "utf-8",
+  });
   res.json({
-    message: "This is the register controller",
+    message: "Register Successful",
   });
   next();
 };
