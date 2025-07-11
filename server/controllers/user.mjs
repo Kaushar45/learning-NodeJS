1f1872fa1c54f5aa5baf9d5a51fa87409f53f909
Kaushar45 <45kaushar@gmail.com>
2025-07-11 06:21:55 +0530

    Enhance input validation and user existence check in registerController

--- server/controllers/user.mjs
+++ server/controllers/user.mjs
@@ -1,4 +1,5 @@
 export const registerController = (req, res, next) => {
+  // Validate input
   if (!req.body.username || !req.body.email || !req.body.password) {
     res.status(400).json({
       error: "Input is not valid.",
@@ -7,19 +8,34 @@ export const registerController = (req, res, next) => {
     return;
   }
 
+  // db file read
   const fileDataStr = readFile("./db.json", { encoding: "utf-8" });
+
+  // parse string to JSON object
   const fileData = JSON.parse(fileDataStr);
 
+  // user data object
   const userData = {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   };
 
+  // Check if user already exists
+  if (fileData.user.some((user) => user.email === userData.email)) {
+    res.status(400).json({
+      error: "User already exists.",
+    });
+    return;
+  }
+
   fileData.user.push(userData);
+
+  // db json update
   writeFile("./db.json", JSON.stringify(fileData), {
     encoding: "utf-8",
   });
+  // Send response
   res.json({
     message: "Register Successful",
   });
